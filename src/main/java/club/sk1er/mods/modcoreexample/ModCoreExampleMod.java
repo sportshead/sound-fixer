b8a3e14e4a77fed508393d3bc4cb19d2e8cc6149
sportshead <32637656+sportshead@users.noreply.github.com>
2021-02-18 10:59:55 +0800

    Initialize mixin

---
+++ src/main/java/club/sk1er/mods/modcoreexample/ModCoreExampleMod.java
@@ -0,0 +1,11 @@
+package club.sk1er.mods.modcoreexample;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ModCoreExampleMod.MODID, version = ModCoreExampleMod.VERSION)
+public class ModCoreExampleMod {
+    public static final String MODID = "modcore_example_mod";
+    public static final String VERSION = "1.0";
+
+
+}
